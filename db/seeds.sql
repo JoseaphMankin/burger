ce12052d6274512a59f323e0fd33a21ada0066cd
Joseaph Mankin <JoseaphMankin@gmail.com>
2018-11-04 09:27:02 -0600

    Intial Block In. Still really buggy

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+-- Insert a set of records.
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger.', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Jelly Bean Milksteak.', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Steamed Hams.', false);
